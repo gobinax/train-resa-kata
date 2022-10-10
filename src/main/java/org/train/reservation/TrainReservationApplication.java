cfe9b80851cd9d906ed828a76334d3ec3e2cf093
Julien Gaubert <gaubert.julien@gmail.com>
2022-10-10 15:32:09 +0200

    Springify

---
+++ src/main/java/org/train/reservation/TrainReservationApplication.java
@@ -0,0 +1,13 @@
+package org.train.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainReservationApplication.class, args);
+    }
+
+}
